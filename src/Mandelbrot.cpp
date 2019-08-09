ce16cc1eb53afc650f2001f9f36aa0f3735af1a1
Aniket kumar <anikketkumar786@gmail.com>
2019-08-09 22:11:43 +0530

    implemented Mandelbrot::getIterations

--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <complex>
 #include "Mandelbrot.h"
+
+using namespace std;
 
 Mandelbrot::Mandelbrot()
 {}
@@ -9,5 +12,16 @@ Mandelbrot::~Mandelbrot()
 {}
 
 int Mandelbrot::getIterations(double x, double y) {
-	return 0;
+	complex<double> z{ 0 };
+	complex<double> c{ x, y };
+	int iterations = 0;
+	while (iterations < MAX_ITERATIONS) {
+		z = z * z + c;
+
+		if (abs(z) > 2)
+			break;
+
+		iterations++;
+	}
+	return iterations;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cstdint>
 #include "Bitmap.h"
+#include "Mandelbrot.h"
 
 using namespace std;
 
@@ -11,11 +13,16 @@ int main() {
 	double max = -999999;
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+			double xFractal = (x - WIDTH/2 - 200) * 2.0/HEIGHT;
 			double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
 
-			if (xFractal < min) min = xFractal;
-			if (xFractal > max) max = xFractal;
+			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+			uint8_t color = (uint8_t)(256*(double)iterations / Mandelbrot::MAX_ITERATIONS);
+			color = color * color * color;
+			bitmap.setPixel(x, y, 0, 0, color);
+
+			if (color < min) min = color;
+			if (color > max) max = color;
 		}
 	}
 	cout << min << ", " << max << endl;
